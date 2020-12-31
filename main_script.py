b80e1e7f7ea8bb6eef87f768e1a80b1e101c2bb0
Harsh Dagar <34965581+HD15Tykoon@users.noreply.github.com>
2020-12-31 23:03:31 +0530

    Add generator that generates list as per user data

--- main_script.py
+++ main_script.py
@@ -1,6 +1,7 @@
 # Initial Python v.3.9.1 used
 
 import os
+import time as t
 
 # This variable will get the current location of the script, in which it has been moved to
 current_directory=os.path.dirname(__file__)
@@ -9,26 +10,48 @@ current_directory=os.path.dirname(__file__)
 dummy_series_path=os.path.join(current_directory,"dummy series")
 
 # Takes the number of seasons available in the current directory to rename files accordingly
-season_number=int(input("Number of Seasons in this folder: "))
+seasons=int(input("Number of Seasons in this folder: "))
 
 # This will check if the number of seasons are more than 1
 ### then program should ask number of episodes in different seasons, this information will be stored in a list
-if season_number>1:
+no_of_files_in_directory=len(os.listdir(dummy_series_path))
+if seasons>1:
     no_of_episode_per_season=[]
-    for _ in range(season_number):
+    for _ in range(seasons):
         no_of_episode_per_season.append(int(input("Number of episodes in Season"+str(_+1)+": ")))
-    
+   
+    print('Verifying...')
+    t.sleep(1)
     # Below statment will check if number of files in the directory and number of episodes provided by user are same
-    assert sum(no_of_episode_per_season)==len(os.listdir(dummy_series_path)), "CONFLICT: NUMBER OF FILES"
+    assert sum(no_of_episode_per_season)==no_of_files_in_directory, "CONFLICT: NUMBER OF FILES"
+else:
+    no_of_episode_per_season=no_of_files_in_directory
 
 """ Available information from user: 
   @var  season_number of type int
-  @var  no_of_episode of type list
+  @var  no_of_episode_per_season of type list
 """
 
+# new name list of names as per the information provided by user
+newNames=list()
+for season_number in range(1,seasons+1):
+    for episode_number in range(1, no_of_episode_per_season[season_number-1]+1):
+        newNames.append("S{0:0=2d}E{0:0=2d}".format(season_number,episode_number))
 
+def nameChanger(source, newName): # Takes the old and new filenames only and rename in actual
+    os.rename(os.path.join(dummy_series_path,source),os.path.join(dummy_series_path,newName))
+
+
+print("Checking Files...")
+t.sleep(1)
+
+print(newNames)
 # This function traverse through the files in a serial manner in the current directory
+print(os.walk(dummy_series_path))
 for root, dirs, files in os.walk(dummy_series_path):
     for filename in files:
-        """NEW CODE SHOULD BE HERE THAT RENAME FILES ACCORDING TO NUMBER OF EPISODES AND NUMBER OF SEASONS"""
-        #CODE HERE*
+        # Just need to renames the files as per newNames list data
+        pass
+        # New Code here but remove pass first
+        #else:
+        #   print("Something went wrong, issue could be due to beta version of script")
