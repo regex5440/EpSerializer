e7772ede2b3f665033c7b93e6cd64cd6ccd42044
Harsh Dagar <34965581+HD15Tykoon@users.noreply.github.com>
2021-01-02 03:34:33 +0530

    Main feature added to rename files

--- main_script.py
+++ main_script.py
@@ -18,7 +18,7 @@ no_of_files_in_directory=len(os.listdir(dummy_series_path))
 if seasons>1:
     no_of_episode_per_season=[]
     for _ in range(seasons):
-        no_of_episode_per_season.append(int(input("Number of episodes in Season"+str(_+1)+": ")))
+        no_of_episode_per_season.append(int(input("Number of episodes in Season "+str(_+1)+": ")))
    
     print('Verifying...')
     t.sleep(1)
@@ -28,15 +28,15 @@ else:
     no_of_episode_per_season=no_of_files_in_directory
 
 """ Available information from user: 
-  @var  season_number of type int
+  @var  seasons of type int
   @var  no_of_episode_per_season of type list
 """
 
 # new name list of names as per the information provided by user
 newNames=list()
-for season_number in range(1,seasons+1):
-    for episode_number in range(1, no_of_episode_per_season[season_number-1]+1):
-        newNames.append("S{0:0=2d}E{0:0=2d}".format(season_number,episode_number))
+for season_number in range(seasons):
+    for episode_number in range(no_of_episode_per_season[season_number]):
+        newNames.append("S{0:0=2d}E{1:0=2d}".format(season_number+1,episode_number+1))
 
 def nameChanger(source, newName): # Takes the old and new filenames only and rename in actual
     os.rename(os.path.join(dummy_series_path,source),os.path.join(dummy_series_path,newName))
@@ -45,13 +45,8 @@ def nameChanger(source, newName): # Takes the old and new filenames only and ren
 print("Checking Files...")
 t.sleep(1)
 
-print(newNames)
 # This function traverse through the files in a serial manner in the current directory
-print(os.walk(dummy_series_path))
 for root, dirs, files in os.walk(dummy_series_path):
-    for filename in files:
-        # Just need to renames the files as per newNames list data
-        pass
-        # New Code here but remove pass first
-        #else:
-        #   print("Something went wrong, issue could be due to beta version of script")
+    for filename,newName in zip(files,newNames):
+        nameChanger(filename,newName)
+        print(filename,'Rename =>',newNames)
