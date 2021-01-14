ed2645fe7b6ca2a3d4bf898c854a96b7f341abe2
Harsh Dagar <34965581+HD15Tykoon@users.noreply.github.com>
2021-01-14 11:49:18 +0530

    add feature to include file types in generator

--- dummy series/dummyFileGenerator.py
+++ dummy series/dummyFileGenerator.py
@@ -2,14 +2,14 @@ import os
 directory=os.path.dirname(os.path.abspath(__file__))
 
 available_files=(len(os.listdir(directory))-1)
-print("\n",available_files,"samples found!")
+print(available_files,"samples found!")
 
 files_required=int(input("\nNumber of sample Required: "))
 
 files_required+=available_files
 
 for _ in range(available_files+1, files_required+1):
-    f=open(os.path.join(os.path.dirname(__file__),"file"+str(_)),"w")
+    f=open(os.path.join(os.path.dirname(__file__),"file"+str(_)+".txt"),"w")
     f.write("file"+str(_))
     f.close
 print(files_required-available_files, "sample created!\n")
